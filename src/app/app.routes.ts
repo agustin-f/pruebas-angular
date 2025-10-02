3fa4c08eddbd400d87741f8f67b3dbc4db6a0c88
agustin-f <farisanoagustin@gmail.com>
2025-10-02 16:11:19 -0300

    update angular hero and counter components and routes bug

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 // Update the import path below to the correct location of CounterComponent
 import { CounterComponent } from './pages/counter/cuonter.component';
+import { heroPageComponent } from './pages/hero/hero-page.component';
 
 export const routes: Routes = [
     {
-        path: '',
+        path: './',
         component: CounterComponent
+    },
+    {
+       path: 'hero',
+       component: heroPageComponent
     }
 ];
