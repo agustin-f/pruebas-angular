c217a121b67ee0ce808f131ef92b44f14094bd46
agustin-f <farisanoagustin@gmail.com>
2025-11-19 15:58:00 -0300

    agrego componente dragonball

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 // Update the import path below to the correct location of CounterComponent
 import { CounterComponent } from './pages/counter/cuonter.component';
 import { heroPageComponent } from './pages/hero/hero-page.component';
+import { dragonballPageComponent } from './pages/dragonball/dragonball-page.component';
 
 export const routes: Routes = [
     {
@@ -11,6 +12,10 @@ export const routes: Routes = [
     {
        path: 'hero',
        component: heroPageComponent
+    },
+    {
+       path: 'dragonball',
+       component: dragonballPageComponent
     },
     {
         path: '**',
