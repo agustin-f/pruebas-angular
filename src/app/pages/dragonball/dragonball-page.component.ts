3d1be3b032cf9841b94ee008d906f51c132f04dd
agustin-f <farisanoagustin@gmail.com>
2025-11-20 08:44:17 -0300

    add for to create dragonball list items

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,4 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, signal, Signal } from "@angular/core";
+
+interface character {
+    id: number;
+    name: string;
+    powerLevel: number;
+}
 
 
 @Component({
@@ -6,5 +12,13 @@ import { Component } from "@angular/core";
     templateUrl: './dragonball-page.component.html',    
 })
 export class dragonballPageComponent {
-    constructor() {}
+    characters: Signal<character[]>;
+
+    constructor() {
+        this.characters = signal ([
+            {id:1, name:'Goku', powerLevel:9002 }, 
+            {id:1, name:'Veggeta', powerLevel:2500 }, 
+            {id:1, name:'Piccolo', powerLevel:3000 } 
+        ]);
+    }
 }
