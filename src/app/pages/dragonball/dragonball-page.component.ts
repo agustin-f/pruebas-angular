46ffa23edf59a4c1baa1e5d37d90d3d59d3c0b0d
agustin-f <farisanoagustin@gmail.com>
2025-11-28 13:39:33 -0300

    agrego signal de name y power

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,24 +1,25 @@
-import { Component, signal, Signal } from "@angular/core";
+import { Component, signal, Signal } from '@angular/core';
 
 interface character {
-    id: number;
-    name: string;
-    powerLevel: number;
+  id: number;
+  name: string;
+  powerLevel: number;
 }
 
-
 @Component({
-    selector: 'app-dragonball-page',
-    templateUrl: './dragonball-page.component.html',    
+  selector: 'app-dragonball-page',
+  templateUrl: './dragonball-page.component.html',
 })
 export class dragonballPageComponent {
-    characters: Signal<character[]>;
+  characters: Signal<character[]>;
+  name = signal('Gohan');
+  powerLevel = signal(100);
 
-    constructor() {
-        this.characters = signal ([
-            {id:1, name:'Goku', powerLevel:9002 }, 
-            {id:1, name:'Veggeta', powerLevel:2500 }, 
-            {id:1, name:'Piccolo', powerLevel:3000 } 
-        ]);
-    }
+  constructor() {
+    this.characters = signal([
+      { id: 1, name: 'Goku', powerLevel: 9002 },
+      { id: 2, name: 'Veggeta', powerLevel: 2500 },
+      { id: 3, name: 'Piccolo', powerLevel: 900 },
+    ]);
+  }
 }
